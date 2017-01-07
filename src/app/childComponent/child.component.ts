96c726aec2e50181b987f86ef4559bc4017e47a8
Saidek <sadekisan@gmail.com>
2017-01-08 00:19:42 +0300

    Test

---
+++ src/app/childComponent/child.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by matve on 08.01.2017.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'child-comp',
+    template: `<ng-content></ng-content>
+                <p>Привет {{name}}</p>`,
+    styles: [`h2, p {color:navy;}`]
+})
+export class ChildComponent {
+    name= "Евгений";
+}
