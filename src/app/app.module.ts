8fa298f1d63a41c650194f403d09e334b55b1ee8
Saidek <sadekisan@gmail.com>
2017-01-07 23:54:09 +0300

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: true })
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {
+
+}
